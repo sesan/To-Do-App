113136262074ec708e43a8e79d93e9ad2c155110
Sesan.Osanyinbi <sesan.osanyinbi@venturegardengroup.com>
2017-08-28 12:14:16 +0100

    Load task from Tasks collection

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -2,13 +2,12 @@
  * Created by Sesan.Osanyinbi on 8/28/2017.
  */
 import {Template} from 'meteor/templating';
+import { Tasks } from '../api/tasks.js';
 
 import './body.html';
 
 Template.body.helpers({
-    tasks: [
-        {text: 'This is Task 1'},
-        {text: 'This is Task 2'},
-        {text: 'This is Task 3'}
-    ],
+   tasks(){
+       return Tasks.find({});
+   }
 });
